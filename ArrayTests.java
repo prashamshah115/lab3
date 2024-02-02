08ca853c08cd9bfb92b82bdd72e184d6e304ba2d
prashamshah115 <155808867+prashamshah115@users.noreply.github.com>
2024-02-01 16:51:50 -0800

    Fixed bugs and added new test cases

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace1() { 
+    int[] input1 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input1);
+  }
+
+  @Test
+public void testReversed1() {
+  int[] input1 = {2,3,4,5};
+  assertArrayEquals(new int[]{5,4,3,2}, ArrayExamples.reversed(input1));
+
+}
 }
